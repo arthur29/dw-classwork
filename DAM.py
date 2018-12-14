8428cd5a8bec6d9a337a8a3545003c5bdb4101a4
Arthur Gonçalves <arthurn2995@gmail.com>
2018-12-13 22:14:33 -0200

    Correct DAM form

--- DAM.py
+++ DAM.py
@@ -14,54 +14,96 @@ engine = create_engine('mysql+mysqlconnector://'+
 Session = sessionmaker(bind=engine)
 session = Session()
 
-def DAM():
-    DAM=[]
+def min_max_trimester(trim):
+    min = 0
+    max = 0
+    if trim == 1:
+        min = 1
+        max = 3
+    elif trim == 2:
+        min = 4
+        max = 6
+    elif trim == 3:
+        min = 7
+        max = 9
+    else:
+        min = 10
+        max = 12
+    return [min,max]
 
-    real_demand = session.query(
-            Order.IDPROD,
+def average_demand():
+    total_orders = session.query(
+        Order.IDPROD,
+            case([
+            (func.month(Order.DATAPED) < 4, 1),
+            (func.month(Order.DATAPED) < 7, 2),
+            (func.month(Order.DATAPED) < 10, 3)
+            ],
+            else_=4),
             func.year(Order.DATAPED),
-            func.month(Order.DATAPED),
             func.sum(Order.QTDE)
         ).group_by(
-            Order.IDPROD,
+            case([
+            (func.month(Order.DATAPED) < 4, 1),
+            (func.month(Order.DATAPED) < 7, 2),
+            (func.month(Order.DATAPED) < 10, 3)
+            ],
+            else_=4),
             func.year(Order.DATAPED),
-            func.month(Order.DATAPED)
+            Order.IDPROD
         ).all()
-    aux_data=[] #data for standard deviation calculation
-    i=0
+    mean_list = []
+    for total in total_orders:
+        #[IDPROD, YEAR, TRIMESTER, AVERAGE_DEMAND]
+        mean_list.append([total[0], total[2], total[1], float(total[3])/3.0])
+    return mean_list
 
-    for demand in real_demand:
-        aux_data.append(demand[3])
-        i+=1
+def DAM(demand_list):
+    DAM=[]
 
-        if(i==3):
-            if(demand[2]<4):
-                tri=1
-            elif(demand[2]<7):
-                tri=2
-            elif(demand[2]<10):
-                tri=3
-            else:
-                tri=4
+    for demand in demand_list:
+        [min,max] = min_max_trimester(demand[2])
+        count_demand = session.query(
+                func.count(Order.QTDE)).filter(
+                Order.IDPROD == demand[0],
+                func.month(Order.DATAPED) >= min,
+                func.month(Order.DATAPED) <= max,
+                func.year(Order.DATAPED) == demand[1]
+                ).group_by(
+                    case([
+                    (func.month(Order.DATAPED) < 4, 1),
+                    (func.month(Order.DATAPED) < 7, 2),
+                    (func.month(Order.DATAPED) < 10, 3)
+                    ],
+                    else_=4),
+                    func.year(Order.DATAPED),
+                    Order.IDPROD
+                ).first()
+        count_demand = count_demand[0]
 
-            DAM.append([statistics.stdev(aux_data),
-                demand[1],
-                tri, demand[0]
-            ])
-            aux_data=[]
-            i=0
+        all_demands = session.query(
+                Order.QTDE).filter(
+                Order.IDPROD == demand[0],
+                func.month(Order.DATAPED) >= min,
+                func.month(Order.DATAPED) <= max,
+                func.year(Order.DATAPED) == demand[1]
+                ).all()
+        absolute_error = 0.0
+        for item in all_demands:
+            absolute_error += item[0] - demand[3]
 
-    for dam in DAM:
-        time = session.query(TimeDimension).filter(TimeDimension.TRIMESTRE == dam[2], TimeDimension.ANO == dam[1]).first()
+        dam = absolute_error / float(count_demand)
+        if dam < 0:
+            dam = dam *-1
+        time = session.query(TimeDimension).filter(TimeDimension.TRIMESTRE == demand[2], TimeDimension.ANO == demand[1]).first()
         if (time is None):
-            time = TimeDimension(TRIMESTRE = dam[2], ANO = dam[1])
+            time = TimeDimension(TRIMESTRE = demand[2], ANO = demand[1])
             session.add(time)
             session.commit()
-        time = session.query(TimeDimension.ID).filter(TimeDimension.TRIMESTRE == dam[2], TimeDimension.ANO == dam[1]).first()
+        time = session.query(TimeDimension.ID).filter(TimeDimension.TRIMESTRE == demand[2], TimeDimension.ANO == demand[1]).first()
 
-        statement = insert(InventoryControlFact). values(IDPROD = dam[3], DAM = float(dam[0]), IDTEMPO = time[0]).on_duplicate_key_update(DAM = float(dam[0]))
+        statement = insert(InventoryControlFact). values(IDPROD = demand[0], DAM = dam, IDTEMPO = time[0]).on_duplicate_key_update(DAM = dam)
         engine.execute(statement)
         session.commit()
-    return DAM
 
-DAM()
+DAM(average_demand())
